f98d71b09c7aa54b4f8865a2577400d7e7982ff5
Ammar Zahid <ammarzahid304@gmail.com>
2023-05-14 22:06:20 +0500

    variables, datatypes and operators

---
+++ Day48-JS-Variables-DataTypes-Operators/app.js
@@ -0,0 +1,24 @@
+var a = 50;
+var b = 83.32;
+var c = "JavaScript";
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+// Types of Operators in Javascript
+// 1- Unary Operators --> Operators with only one operand
+// 2- Binary Operators --> Operators with two operands
+
+var num1 = 23;
+console.log(++num1);
+console.log(num1++);
+console.log(num1);
+
+var num2 = 5;
+
+console.log('num1 + num2 = ' + (num1 + num2));
+console.log('num1 - num2 = ' + (num1 - num2));
+console.log('num1 * num2 = ' + (num1 * num2));
+console.log('num1 / num2 = ' + (num1 / num2));
+console.log('num1 ** num2 = ' + (num1 ** num2));
