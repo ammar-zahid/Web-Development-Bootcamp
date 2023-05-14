49b0657e481766f70ded8d4c3a28fc6083a52fb7
Ammar Zahid <ammarzahid304@gmail.com>
2023-05-14 22:30:41 +0500

    Strings in JavaScript

---
+++ Day49-JS-Strings-in-JavaScript/app.js
@@ -0,0 +1,21 @@
+var name = 'ammar';
+var grade = ' is in class 10 ';
+var learning = 'he is learning javascript';
+
+console.log(name);
+console.log(grade);
+console.log(learning);
+
+console.log(name + grade + learning);
+
+var greetings = `Hello Mr ${name} "Good Morning"`;
+console.log(greetings);
+
+var subject = new String ('Philosophy is a boring subject');
+console.log(subject);
+
+var len = name.length;
+console.log(len);
+
+var len2 = subject.length;
+console.log(len2);
