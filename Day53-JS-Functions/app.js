f939ebc1184ef3ce50d65096cb499d2d4893a250
Ammar Zahid <ammarzahid304@gmail.com>
2023-05-16 16:40:13 +0500

    Functions in JavaScript

---
+++ Day53-JS-Functions/app.js
@@ -0,0 +1,32 @@
+function greet(name) {
+    console.log('Your name is ' + name);
+}
+
+let name = 'Ammar';
+let name2 = 'Harry';
+let name3 = 'Carry';
+
+greet(name2);
+
+let code = 'Java';
+let code1 = 'JavaScript';
+let code2 = 'C#';
+function learn(code) {
+    console.log('You are learning ' + code);
+}
+
+learn(code1);
+
+function sum(a, b, c) {
+    let d = a + b + c;
+    return d;
+}
+
+// let answer = sum(2,5,3);
+// console.log('Answer is ' + answer);
+let a = 2;
+let b = 5;
+let c = 9;
+
+let answer = sum(a,b,c);
+console.log('Answer is ' + answer);
