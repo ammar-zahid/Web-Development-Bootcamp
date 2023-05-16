57d9fa367e01c5bfb99cedd5daa59c2004e8cf29
Ammar Zahid <ammarzahid304@gmail.com>
2023-05-16 21:42:47 +0500

    Loops in Javascript

---
+++ Day55-JS-Loops/app.js
@@ -0,0 +1,28 @@
+for(let i = 0; i <= 10; i++) {
+    console.log(i);
+}
+
+let string = ["Ammar", "Javascript", "Youtube"];
+
+string.forEach(function print(elements){
+    console.log(elements);
+});
+
+string.forEach(element => {
+    console.log(element);
+});
+
+let i = 0;
+while (i < 5) {
+    console.log(i);
+    i++;
+}
+
+// let i = 1;
+// const n = 5;
+
+// do...while loop from 1 to 5
+// do {
+//     console.log(i);
+//     i++;
+// } while(i <= n);
