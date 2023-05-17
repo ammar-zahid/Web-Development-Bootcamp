b1e9f928a0bbc1e393343690ee87be1d4b6f7da9
Ammar Zahid <ammarzahid304@gmail.com>
2023-05-17 11:58:56 +0500

    Navigating DOM

---
+++ Day56-JS-Navigating-DOM/app.js
@@ -0,0 +1,17 @@
+let list = document.getElementById('list');
+console.log(list);
+
+let nav = document.getElementById('nav');
+console.log(nav);
+
+let container = document.getElementsByClassName('container');
+console.log(container);
+
+let sel = document.querySelector('#list>ul');
+console.log(sel);
+
+let sel2 = document.querySelector('.container');
+console.log(sel2);
+
+let sel3 = document.querySelector('.container>p');
+console.log(sel3);
