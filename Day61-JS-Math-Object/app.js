eb37263871eaca6b365e69fbeb6af5edf717ba17
Ammar Zahid <ammarzahid304@gmail.com>
2023-05-17 21:20:02 +0500

    Math Library

---
+++ Day61-JS-Math-Object/app.js
@@ -0,0 +1,48 @@
+let m = Math;
+// console.log(m);
+// E: 2.718281828459045
+// LN2: 0.6931471805599453
+// LN10: 2.302585092994046
+// LOG2E: 1.4426950408889634
+// LOG10E: 0.4342944819032518
+// PI: 3.141592653589793
+// SQRT1_2: 0.7071067811865476
+// SQRT2: 1.4142135623730951
+
+console.log(`The value of Math.PI is ${Math.PI}`);
+console.log(`The value of Math.LN2 is ${Math.LN2}`);
+console.log(`The value of Math.LOG2E is ${Math.LOG2E}`);
+console.log(`The value of Math.LOG10E is ${Math.LOG10E}`);
+console.log(`The value of Math.SQRT2 is ${Math.SQRT2}`);
+
+let x = 33.33;
+let y = 98.83;
+let z = 67.49;
+
+console.log('The value of x is ' + x);
+console.log('The value of y is ' + y);
+console.log('The value of z is ' + z);
+console.log('The value of rounded x is ' + Math.round(x));
+console.log('The value of rounded y is ' + Math.round(y));
+console.log('The value of rounded z is ' + Math.round(z));
+
+console.log('3 raised to the power 2 = ' + Math.pow(3,2));
+console.log('5 raised to the power 5 = ' + Math.pow(5,5));
+console.log('9 raised to the power 7 = ' + Math.pow(9,7));
+
+console.log('square root of 25 = ' + Math.sqrt(25));
+console.log('square root of 16 = ' + Math.sqrt(16));
+console.log('square root of 81 = ' + Math.sqrt(85));
+
+console.log('6.8 is round up to ' + Math.ceil(6.8));
+console.log('6.8 is round down to ' + Math.floor(6.8));
+
+console.log('The absolute value of 5.66 is ' + Math.abs(5.66));
+console.log('The absolute value of -5.66 is ' + Math.abs(-5.66));
+
+console.log('Value of sin(45) is ' + Math.sin(45));
+console.log('Value of cos(45) is ' + Math.cos(45));
+console.log('Value of tan(45) is ' + Math.tan(45));
+
+console.log('Minimum number from (5,2,7) is ' + Math.min(5,2,7));
+console.log('Maximum number from (5,2,7) is ' + Math.max(5,2,7));
